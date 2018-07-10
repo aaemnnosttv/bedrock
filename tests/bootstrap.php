2022e778d8218f601e5e00e71562b92115f18112
Evan Mattson <me@aaemnnost.tv>
2018-07-11 00:07:21 +0200

    use env() in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,11 +8,11 @@
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 // Give access to tests_add_filter() function.
-require_once(getenv('WP_PHPUNIT__DIR') . '/includes/functions.php');
+require_once(env('WP_PHPUNIT__DIR') . '/includes/functions.php');
 
 tests_add_filter('muplugins_loaded', function () {
     // test set up, plugin activation, etc.
 });
 
 // Start up the WP testing environment.
-require(getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php');
+require(env('WP_PHPUNIT__DIR') . '/includes/bootstrap.php');
